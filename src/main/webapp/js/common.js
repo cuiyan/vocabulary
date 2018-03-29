a429add54b60e71022c8f4ac7915d22183b85cdf
cuiyan <cuiyan@tusvn.com>
2018-03-29 16:05:11 +0800

    modify html

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,4 +1,4 @@
-var baseurl = "http://localhost:9090/vocabulary/";
+var baseurl = "http://192.168.12.86:9090/vocabulary/";
 function getUrlParam(key) {  
     var reg = new RegExp(key + '=([^&]*)');  
     var results = location.href.match(reg);  
